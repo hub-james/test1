6335929f24b83a7783bc0bd0c9ee7995d570e194
hub-james <nttdatatj@foxmail.com>
2021-07-30 16:57:37 +0900

    test1

---
+++ src/main/java/ntt/data/TestApplication.java
@@ -0,0 +1,11 @@
+package ntt.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class,args);
+    }
+}
---
+++ src/main/java/ntt/data/controller/TestController.java
@@ -0,0 +1,13 @@
+package ntt.data.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/Hello")
+    public String Test(){
+        return "TestController";
+    }
+}
